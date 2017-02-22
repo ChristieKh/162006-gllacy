a45716714ac7cdb28754aec6b966104aecf2cb7b
ChristieKh <kharushina.christina@gmail.com>
2017-02-22 05:18:20 +0500

    Финальная версия

--- js/myscript.js
+++ js/myscript.js
@@ -1,20 +1,21 @@
-        var link = document.querySelector(".btn-map");
-        var popup = document.querySelector(".modal-content");
-        var close = popup.querySelector(".modal-content-close");
-        
-        
-        link.addEventListener("click", function(event) {
-            event.preventDefault();
-            popup.classList.add("modal-content-show");
-            login.focus();
-        });
-        close.addEventListener("click", function(event) {
-        event.preventDefault();
-        popup.classList.remove("modal-content-show");  });
-         window.addEventListener("keydown", function(event) {
-        if (event.keyCode === 27) {
-          if (popup.classList.contains("modal-content-show")) {
+var link = document.querySelector(".btn-map");
+var popup = document.querySelector(".modal-content");
+var close = popup.querySelector(".modal-content-close");
+
+
+link.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.add("modal-content-show");
+    login.focus();
+});
+close.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("modal-content-show");
+});
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        if (popup.classList.contains("modal-content-show")) {
             popup.classList.remove("modal-content-show");
-          }
         }
-      });
+    }
+});
